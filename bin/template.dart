53419dfa54971b37eab6ed99f30c407afdd67181
psyonixFx <yousuf.mg.hammad@gmail.com>
2022-01-21 05:26:43 +0000

    CI build

--- bin/template.dart
+++
@@ -1,3 +0,0 @@
-const String template = '''
-just long text 
-''';
--- lib/material_theme_builder.dart
+++
@@ -1,5 +0,0 @@
-library material_theme_builder;
-
-export 'src/parser.dart';
-export 'src/model.dart';
-export 'src/extemsion.dart';
